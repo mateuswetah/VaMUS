ddeea0fa90a20ce7683d1fdf62d0b99aaa0ff1ba
mateuswetah <mateus.m.luna@gmail.com>
2020-10-22 23:25:14 -0300

    feat: Adds axios and vuex.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/essential',
     'eslint:recommended',
     '@vue/typescript/recommended'
   ],
@@ -12,10 +12,9 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/no-deprecated-slot-attribute': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
   },
   overrides: [
     {
