421c8b33b0a8ff2051b361686dfbf558bb70e7d4
mateuswetah <mateus.m.luna@gmail.com>
2020-10-24 01:25:44 -0300

    fix: tainacan api endpoint error

--- src/store/axios.ts
+++ src/store/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Tainacan API Axios
 export const tainacanApi = axios.create({
-    baseURL: 'http://vamus.tainacan.org/wp-json/tainacan/v2'
+    baseURL: 'https://vamus.tainacan.org/wp-json/tainacan/v2'
 });
 
 export default { tainacanApi };
