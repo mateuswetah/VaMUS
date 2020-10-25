eb00256e482d0d64492e5195d8c1c67ac565dfbb
vnmedeiros <vnicius.nm.ba@gmail.com>
2020-10-24 21:31:47 -0300

    feat: add point (institutes, collections, items) in map

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -19,6 +19,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { mapGetters } from "vuex";
 import {
     IonPage,
     IonHeader,
@@ -33,23 +34,96 @@ export default defineComponent({
     components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
     data(): {
         map: L.Map | undefined;
+        searchKey: string;
+        points: {id: number; lat: number; long: number; markerType: string}[];
     } {
         return {
             map: undefined,
+            searchKey: '',
+            points: [ ],
         };
     },
+    computed: {
+        ...mapGetters("institute", {
+            institutes: "getInstitutes",
+        }),
+        ...mapGetters("collection", {
+            collections: "getCollections",
+        }),
+        ...mapGetters("item", {
+            items: "getItems",
+        }),
+    },
     methods: {
+        getIconUrlMarker(type: string) {
+            switch (type) {
+                case '001': return 'assets/icon/marker-icon-1.png';
+                case '010': return 'assets/icon/marker-icon-2.png';
+                case '100': return 'assets/icon/marker-icon-3.png';
+                case '101': return 'assets/icon/marker-icon-4.png';
+                case '011': return 'assets/icon/marker-icon-5.png';
+                case '110': return 'assets/icon/marker-icon-8.png';
+                case '111': return 'assets/icon/marker-icon-7.png';
+                default: return 'assets/icon/marker-icon-1.png';
+            }
+        },
+        populatePoints() {
+            this.points = [];
+            for (let index = 0; index < (this.institutes||[]).length; index++) {
+                const element = this.institutes[index];
+                const {id, location: {latitude, longitude}} = element;
+                this.points.push({id, lat:latitude, long:longitude, markerType: '001'})
+            }
+
+            for (let index = 0; index < (this.collections||[]).length; index++) {
+                const element = this.collections[index];
+                const id = element.vamus_institute_identifier_collection;
+                const latitude  = element.vamus_institute_lat_collection;
+                const longitude = element.vamus_institute_long_collection;
+                const current = this.points.find(x => x.id == id);
+                if (current){
+                    current.markerType = current.markerType.substring(0,1) + '1' + current.markerType.substring(2);
+                } else {
+                    this.points.push({id, lat:latitude, long:longitude, markerType: '010'})
+                }
+            }
+
+            for (let index = 0; index < (this.items||[]).length; index++) { 
+                const element = this.items[index];
+                const id = element.vamus_institute_identifier_collection;
+                const latitude  = element.vamus_institute_lat_collection;
+                const longitude = element.vamus_institute_long_collection;
+                const current = this.points.find(x => x.id == id);
+                if (current){
+                    current.markerType = '1' + current.markerType.substring(1);
+                } else {
+                    this.points.push({id, lat:latitude, long:longitude, markerType: '100'})
+                }
+            }
+        },
         ionViewDidEnter() {
+            this.populatePoints();
             this.map = L.map("mapId").setView([-15.809365, -49.521065], 5);
 
             L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-                attribution: "edupala.com © Angular LeafLet",
+                attribution: "Instituições culturais",
             }).addTo(this.map);
 
-            L.marker([-15.8, -49.52])
+            for (let i = 0; i < this.points.length; i++) {
+                const defaultIcon = L.icon({
+                    iconUrl: this.getIconUrlMarker(this.points[i].markerType),
+                    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+                    iconSize: [14, 26],
+                    iconAnchor: [8, 26],
+                    popupAnchor: [-3, -38],
+                    shadowSize: [14, 26],
+                    shadowAnchor: [4, 26]
+                });
+
+                L.marker([this.points[i].lat, this.points[i].long], {icon:defaultIcon})
                 .addTo(this.map)
-                .bindPopup("Algum museu")
-                .openPopup();
+                .bindPopup("PopUp")
+            }
         },
         ionViewWillLeave() {
             if (this.map) this.map.remove();
